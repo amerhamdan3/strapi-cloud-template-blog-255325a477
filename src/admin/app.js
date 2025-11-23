c55e7015930cc367957bada571085bb66ed04a1d
Amer hamdan <amer.m.hamdan@gmail.com>
2025-11-23 05:26:10 +0300

    feat: Customize admin panel welcome message translations.

---
+++ src/admin/app.js
@@ -0,0 +1,13 @@
+export default {
+    config: {
+        translations: {
+            en: {
+                "Auth.form.welcome.title": "Welcome to MFA CMS Admin",
+                "Auth.form.welcome.subtitle": "Login into MFA account",
+            },
+        },
+    },
+    bootstrap(app) {
+        console.log(app);
+    },
+};
